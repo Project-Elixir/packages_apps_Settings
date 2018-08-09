df906c7fe75aee900cc139c447e108329bf3c9ee
Christian Oder <myself5@carbonrom.org>
2018-08-09 15:53:22 +0200

    Settings: Add Project Elixir Version to Firmware version window
    Change-Id: I97dfbb070bacc5b8bc4c03751bef9f319a883cbc
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/ElixirVersionDetailPreferenceController.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ * Copyright (C) 2019-2021 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.SystemClock;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.slices.Sliceable;
+import com.android.settingslib.RestrictedLockUtils;
+import com.android.settingslib.RestrictedLockUtilsInternal;
+
+public class ElixirVersionDetailPreferenceController extends BasePreferenceController {
+
+    private static final String TAG = "ElixirVersionDialogCtrl";
+    private static final int DELAY_TIMER_MILLIS = 500;
+    private static final int ACTIVITY_TRIGGER_COUNT = 3;
+
+    private static final String KEY_ELIXIR_VERSION_PROP = "org.elixir.version";
+
+    public ElixirVersionDetailPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return SystemProperties.get(KEY_ELIXIR_VERSION_PROP,
+                mContext.getString(R.string.unknown));
+    }
+}
