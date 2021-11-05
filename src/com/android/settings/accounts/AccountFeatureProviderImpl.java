0dd2851843b4c59f496e5b445c16ebb3f76fa038
jhonboy121 <alfredmathew05@gmail.com>
2021-11-05 15:24:38 +0530

    Settings: complete the AvatarViewMixin implementation
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- src/com/android/settings/accounts/AccountFeatureProviderImpl.java
+++ src/com/android/settings/accounts/AccountFeatureProviderImpl.java
@@ -1,6 +1,7 @@
 package com.android.settings.accounts;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 
 public class AccountFeatureProviderImpl implements AccountFeatureProvider {
@@ -11,6 +12,6 @@ public class AccountFeatureProviderImpl implements AccountFeatureProvider {
 
     @Override
     public Account[] getAccounts(Context context) {
-        return new Account[0];
+        return AccountManager.get(context).getAccounts();
     }
 }
